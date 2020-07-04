c5603aabaf21dc03db95db65f3f828d95009b7d3
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MBP.home>
2020-07-04 20:56:43 +0100

    Updates

---
+++ precache-manifest.e894d3f0d9a61769aa48ef697201a9ea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943377b42fd30f5f81da25c469c095da",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "b67b4cf9f037c67d28b4",
+    "url": "/RandomQuoteMachine/static/css/2.1c4a245c.chunk.css"
+  },
+  {
+    "revision": "439c578b44468629d25c",
+    "url": "/RandomQuoteMachine/static/css/main.7c623d5a.chunk.css"
+  },
+  {
+    "revision": "b67b4cf9f037c67d28b4",
+    "url": "/RandomQuoteMachine/static/js/2.b1c3814b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RandomQuoteMachine/static/js/2.b1c3814b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439c578b44468629d25c",
+    "url": "/RandomQuoteMachine/static/js/main.273f85af.chunk.js"
+  },
+  {
+    "revision": "b31ac83bfc81b443f447",
+    "url": "/RandomQuoteMachine/static/js/runtime-main.b86b2c27.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RandomQuoteMachine/static/media/logo.5d5d9eef.svg"
+  }
+]);
